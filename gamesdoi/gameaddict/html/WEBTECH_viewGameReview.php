3c53646aa30de9c8759a2879e2aeade10234fc8e
zeedome <32031509+zeedome@users.noreply.github.com>
2017-11-20 00:10:29 +0800

    viewGameReview so u can add comment

--- gamesdoi/gameaddict/html/WEBTECH_viewGameReview.php
+++ gamesdoi/gameaddict/html/WEBTECH_viewGameReview.php
@@ -73,10 +73,14 @@
 
 		<div class="page normal-page container">
 		<div >
+			<div class="block block-contactform_block span12 first cf">
+				<div class="title-wrapper">
+					<h3 class="widget-title">Game</h3>
+						<div class="clear"></div>
+					</div>
+				</div>
 		<div class="span12" style=" border-bottom: 1px solid black; margin-bottom: 20px;">
-			<div class="span6" style="margin-bottom: 20px;">
-				<img src="bioshockView.jpg">
-			</div>
+
 			<div style="font-weight: bold; font-size: 28px; float: right; padding-right: 40px; padding-top: 20px; font-family: Open Sans;">Rating: 3.7</div>
 			<div class="span4">
 				<h1>Bioshock</h1>
@@ -88,40 +92,56 @@
 			<br>
 			</div>
 		</div>
-
-
-
 			<br>
 
 		</div>
 
+		<div class="span12" style=" border-bottom: 1px solid black; margin-bottom: 20px;">
+			<h1>TITLE OF REVIEW</h1>
+			<label>BY : *NAME OF REVIEWER</label>
+			*INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE*
+			*INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE*
+			*INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE* *INSERT THE CONTENTS OF REVIEW HERE*
+		</div>
+
 		<div class="block block-contactform_block span12 first cf">
 			<div class="title-wrapper">
-				<h3 class="widget-title">YOUR REVIEW</h3>
+				<h3 class="widget-title">Comments</h3>
 					<div class="clear"></div>
 				</div>
 					<div class="mcontainer">
 				<form action="WEBTECH_viewGame.html" id="contactForm" class="contact" method="post" novalidate="novalidate">
-
 					<ul class="contactform controls">
-
-						<i style="position: relative; bottom: 10px; font-family: Open Sans; font-size: 15px;">Your Rating:</i>
-
 						<li class="input-prepend">
+						<div class="wcontainer">
+							<img src="icon.png">
+							<a href="WEBTECH_userProfile.html"><b class="namereview"> &nbsp;&nbsp;&nbsp;MarvinLA0</b></a>&nbsp; <i> rated it 5 out of 5 </i> <br>
 
-							<fieldset class="rating">
-    						<input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-    						<input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-    						<input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-    						<input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-   							<input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
-							</fieldset>
 
-						</li>
 
-                       	<li class="input-prepend">
+							<div class="ratereview">
+
+								<a class="ups" href="#"><i class="icon-thumbs-up m"></i></a>
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								<a href="#"><i class="icon-thumbs-down m"></i></a>
+
+							</div>
+
+							<div style="padding-left: 50px;">
+								<p class="comment">NICE GAME!!!!</p>
+							</div><p>
+
+							<div class="reviewscore">
+
+								<p><i>Review Score:&nbsp; <b class="posi">+2</b>&nbsp; | &nbsp;<b class="nega">-0</b></i></p>
+
+							</div>
+
+						</div>
+					</li>
+			   		<li class="input-prepend">
 							<span class="add-on"><i class="icon-align-justify"></i></span>
-							<textarea name="comments" placeholder="Write your review..." id="commentsText" rows="7" cols="30" class="required requiredField"></textarea>
+							<textarea name="comments" placeholder="comment something" id="commentsText" rows="7" cols="30" class="required requiredField"></textarea>
 						</li>
 
 						<li>
