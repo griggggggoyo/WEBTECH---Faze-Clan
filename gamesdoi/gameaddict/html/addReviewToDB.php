ed151bf1dbc33bf45514703c00040c4ffa1a5a84
zeedome <32031509+zeedome@users.noreply.github.com>
2017-11-20 16:58:48 +0800

    addReview mo to

---
+++ gamesdoi/gameaddict/html/addReviewToDB.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+$rating = $_POST['rating'];
+$review = $_POST['comments'];
+$userID = $_SESSION["userID"];
+$gameid = $_SESSION['gameid'];
+$title = $_POST['title'];
+
+if($rating == '' || $review == '' || $title == ''){
+    echo "<script type='text/javascript'>alert('Please Enter All Fields');</script>";
+    header("Location:addReview.php");
+}
+
+else{
+    $conn = new mysqli("localhost", "webtech", "", "reviewschema");
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            echo "failed";
+        } 
+        echo "Connected successfully";
+
+    $sql = "INSERT INTO review (gameTitle, reviewRating, reviewText, gameID, userID)
+    VALUES ('$title', $rating, '$review', gameid, userID)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+        header("Location:addReview.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+?>
